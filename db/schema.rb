5401e9355aeb86e15c313aaeda029afd2a39e867
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-01 23:05:35 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,10 @@
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.integer "age"
+    t.string "hometown"
   end
+
 end
