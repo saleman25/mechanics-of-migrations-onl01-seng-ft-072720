35fc2fb3a3d58df4b73f7318672b5b33634d2a38
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-01 23:00:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,9 +1,10 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   def change
-     create table :artists do |t|
+    create_table :artists do |t|
       t.string :name
       t.string :genre
       t.integer :age
       t.string :hometown
+    end
   end
 end
